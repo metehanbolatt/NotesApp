3bc0c92026042bf837a424a72d2c7f3060ec31f6
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-07-13 11:32:12 +0300

    Initial project was completed.

---
+++ app/src/main/java/com/metehanbolat/notesapp/NoteFragment.kt
@@ -0,0 +1,34 @@
+package com.metehanbolat.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.metehanbolat.notesapp.databinding.FragmentNoteBinding
+
+class NoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
